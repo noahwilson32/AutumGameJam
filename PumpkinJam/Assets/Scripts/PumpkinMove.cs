91cd566e866f48fc45484b3dab5399fa16b338f4
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-09-01 09:36:13 -0400

    Getting Started

---
+++ PumpkinJam/Assets/Scripts/PumpkinMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinMove : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+    }
+}

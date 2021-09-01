2cc5d19c31be6da2c5ea67e26ccb1a72c6ce6aa6
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-09-01 17:56:21 -0400

    Spawner

---
+++ PumpkinJam/Assets/Scripts/SpawnWitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWitch : MonoBehaviour
+{
+    public GameObject witchPrefab;
+
+    public bool yesSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yesSpawn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (yesSpawn)
+        {
+            StartCoroutine(Spawn());
+            yesSpawn = false;
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2.5f);
+
+        float rY = Random.Range(-10f, 10f);
+        yesSpawn = true;
+
+        GameObject witchInstance = Instantiate(witchPrefab, new Vector3(transform.position.x,rY,transform.position.z), Quaternion.identity);
+    }
+}
